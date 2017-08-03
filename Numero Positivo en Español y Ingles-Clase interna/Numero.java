d3bdbf89450444868b603959ca56379ccb05e2b6
Marcos <jigoku_shujin@hotmail.com>
2017-08-03 00:10:26 -0500

    Pequeño ejemplo sobre clases internas
    Se que el problema no justifica usarla pero  la use como un pequeño demostrativo numeros es una clase que usara la unica que usara numerosEnInglesYEspañol que contiene un metodo que muestra en pantalla el numero digito en español y en ingles.

---
+++ Numero Positivo en Español y Ingles-Clase interna/Numero.java
@@ -0,0 +1,104 @@
+/*
+ * Numero.java
+ * Copyright (C) 2017 Marcos <jigoku_shujin@hotmail.com>
+ *
+ * Distributed under terms of the S/L license.
+ */
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/*
+ * Pequeño ejemplo en java sobre una clase anidada
+ *
+ */
+
+public class Numero
+{
+    private Scanner entrada;
+    private NumerosIE numerosEIE; // EIE : En Ingles y Español
+
+    public Numero()
+    {
+        this.entrada = new Scanner(System.in);
+        this.numerosEIE = new NumerosIE();
+    }
+
+    public void init()
+    {
+       try
+       {
+            int numeroUsuario = this.datoUsuario();
+            this.numerosEIE.numeroEnEyI(numeroUsuario);
+            
+            this.ordenAcendente(numeroUsuario);
+            this.ordenDescendente(numeroUsuario);
+       }
+
+       catch(ArrayIndexOutOfBoundsException e)
+       {
+           System.out.println("El número no esta en un rango del 1 al 10.");
+       }
+
+       catch(InputMismatchException e)
+       {
+           System.out.println("El número no es dato de tipo numerico entero.");
+       }
+
+       finally
+       {
+           this.entrada.close();
+       }
+    }
+
+    private int datoUsuario()
+    {
+        System.out.print("Escriba un digito númerico positivo:");
+        return this.entrada.nextInt();
+    }
+
+    private void ordenAcendente(int numero)
+    {
+        StringBuffer str = new StringBuffer("Orden ascendete:");
+        for (int i = 1; i <= numero; i++) str.append(i);
+
+        System.out.println(str);
+    }
+
+    private void ordenDescendente(int numero)
+    {   
+        StringBuffer str = new StringBuffer("Orden decendente:");
+        for (int i = numero; i > 0; i--) str.append(i);
+        
+        System.out.println(str);
+    }
+
+    private class NumerosIE
+    {
+        private String numeros[];
+
+        public NumerosIE()
+        {   
+            this.numeros = new String[10];
+            this.setNumerosIE();
+        }
+
+        public void setNumerosIE()
+        {
+            this.numeros[0] = "Uno\nOne";
+            this.numeros[1] = "Dos\nTwo";
+            this.numeros[2] = "Tres\nThree";
+            this.numeros[3] = "Cuatro\nFour";
+            this.numeros[4] = "Cinco\nFive";
+            this.numeros[5] = "Seis\nSix";
+            this.numeros[6] = "Siete\nSeven";
+            this.numeros[7] = "Ocho\nEigth";
+            this.numeros[8] = "Nueve\nNine";
+            this.numeros[9] = "Diez\nTeen";
+        }
+
+        public void numeroEnEyI(int subindice)
+        {
+            System.out.println(this.numeros[subindice - 1]);
+        }
+    };
+}
---
+++ Numero Positivo en Español y Ingles-Clase interna/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Principal.java
+ * Copyright (C) 2017 Marcos <jigoku_shujin@hotmail.com>
+ *
+ * Distributed under terms of the S/L license.
+ */
+
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        Numero num = new Numero();
+        num.init();       
+    }
+}
+
