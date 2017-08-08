4a87bb979319041d2e555561cf89473420fe6f4c
ankToken <ricardocardona.rc@gmail.com>
2017-08-07 23:49:50 -0500

    HolaMundo.c
    HolaMundo en C.

---
+++ HolaMundo.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : HolaMundo.c
+ Author      : Token
+ Version     :
+ Copyright   : Done by Token
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("!!!HolaMundo!!!!!!!!"); /* prints !!!Hello World!!! */
+	return 0;
+}
