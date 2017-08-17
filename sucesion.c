24abd05babf10a25a6b942bf597d0a35ac84dc44
marcos-ro <jigoku_shujin@hotmail.com>
2017-08-17 18:07:14 -0500

    Sucesion numerica en C

---
+++ sucesion.c
@@ -0,0 +1,35 @@
+#include <stdio.h> // Cabecera que contiene la entrada y salida estandar
+
+/*
+ *  Programa que te da el resultado de una sucesion numerica
+ *  Ejemplo: 1 + 2 + 3 = 6.
+ *
+ *  En este caso usare la recursividad.
+ */
+
+int sucesion(int n)
+{
+    if(n == 1)
+    {
+        n = 1;
+    }
+
+    else
+    {
+        n += sucesion(n - 1);
+    }
+
+    return n;
+}
+
+int main()
+{
+    int numero;
+
+    printf("n + ");
+    scanf("%d", &numero);
+
+    printf("Resultado de la sucesion es %d\n", sucesion(numero));
+
+    return 0;
+}
